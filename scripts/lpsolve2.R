5eb4091f69628cf42f7a0836df2ef0c4e7c3ae54
soodoku <gsood07@gmail.com>
2023-06-25 23:11:53 -0700

    Update lpsolve2.R

--- scripts/lpsolve2.R
+++ scripts/lpsolve2.R
@@ -1,9 +1,9 @@
 library(lpSolveAPI)
-set.seed(31415)
+
 # Define the parameters
-n <- 40  # Number of respondents
-m <- 20000  # Number of domains
-epsilon <- 0.1  # Maximum standard error
+n <- 100  # Number of respondents
+m <- 200  # Number of domains
+epsilon <- 0.01  # Maximum standard error
 
 # Generate fake data
 cij <- matrix(rpois(n * m, lambda = 10), ncol = m)  # Visitation matrix
@@ -11,32 +11,27 @@ tj <- sample(c(0, 1), m, replace = TRUE)  # Trait vector
 
 # Create the LP model
 lp_model <- make.lp(0, m)
-
-# Set the objective function (minimize 0)
 set.objfn(lp_model, rep(0, m))
+set.type(lp_model, 1:m, "binary")
 
 # Add constraints for each respondent
 for (i in 1:n) {
-  constraint <- rep(0, m)
-  constraint[(i - 1) * m + 1:(i * m)] <- 1
+  constraint <- cij[i, ]
   add.constraint(lp_model, constraint, ">=", 1)
 }
 
-# Calculate the standard error constraint
-se_constraint <- sqrt(colSums(cij^2)) / sqrt(sum(cij^2 * tj))
-se_constraint_extended <- rep(se_constraint, each = n)
-
 # Add constraint for standard error
-add.constraint(lp_model, se_constraint_extended, "<=", epsilon)
-
-# Set variable type (binary)
-set.type(lp_model, 1:m, "binary")
+for (j in 1:m) {
+  constraint <- matrix(0, nrow = 1, ncol = m)
+  constraint[1, j] <- sqrt(sum(cij[, j]))
+  add.constraint(lp_model, constraint, "<=", epsilon)
+}
 
 # Solve the LP problem
-solve(lp_model)
+status <- solve(lp_model)
 
 # Check if the LP problem is feasible
-if (get.status(lp_model) != 0) {
+if (status != 0) {
   stop("No feasible solution found.")
 }
 
@@ -44,18 +39,12 @@ if (get.status(lp_model) != 0) {
 solution <- get.variables(lp_model)
 
 # Get the selected domains for each respondent
-Di <- lapply(1:n, function(i) which(solution[(i - 1) * m + 1:(i * m)] > 0))
+selected_domains <- lapply(1:n, function(i) which(solution[(i - 1) * m + 1:i * m] > 0))
 
 # Calculate the standard error for each respondent
-se <- sapply(Di, function(d) {
-  if (length(d) > 0) {
-    sqrt(sum(tj[d]) / length(d))
-  } else {
-    NaN
-  }
-})
+standard_error <- sapply(selected_domains, function(d) sqrt(sum(cij[, d])))
 
 # Output the selected domains and standard error for each respondent
 for (i in 1:n) {
-  cat("Respondent", i, ": Selected Domains =", Di[[i]], "Standard Error =", se[i], "\n")
+  cat("Respondent", i, ": Selected Domains =", selected_domains[[i]], "Standard Error =", standard_error[i], "\n")
 }
