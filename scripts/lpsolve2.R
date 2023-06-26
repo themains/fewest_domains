051d88def7378d4076c0f6efecd18f0f4ea2dc64
soodoku <gsood07@gmail.com>
2023-06-25 22:55:33 -0700

    Update lpsolve2.R

--- scripts/lpsolve2.R
+++ scripts/lpsolve2.R
@@ -1,48 +1,61 @@
-library(lpSolve)
+library(lpSolveAPI)
+set.seed(31415)
+# Define the parameters
+n <- 40  # Number of respondents
+m <- 20000  # Number of domains
+epsilon <- 0.1  # Maximum standard error
 
-# Define the number of respondents and domains
-n <- 700 # Number of respondents
-m <- 1000  # Number of domains
+# Generate fake data
+cij <- matrix(rpois(n * m, lambda = 10), ncol = m)  # Visitation matrix
+tj <- sample(c(0, 1), m, replace = TRUE)  # Trait vector
 
-# Set the maximum standard error
-epsilon <- 0.05
+# Create the LP model
+lp_model <- make.lp(0, m)
 
-# Generate example data for number of visits with Poisson distribution
-lambda <- 5  # Mean for Poisson distribution
-cij <- matrix(rpois(n * m, lambda), ncol = m)
-
-# Create an LP model
-lp_model <- make.lp(nrow = n * m + m, ncol = m)
-
-# Set the objective function to minimize the number of unique domains selected
-obj <- rep(0, m)
-set.objfn(lp_model, obj)
+# Set the objective function (minimize 0)
+set.objfn(lp_model, rep(0, m))
 
 # Add constraints for each respondent
 for (i in 1:n) {
-  constraint <- matrix(0, nrow = 1, ncol = m)
-  constraint[1, ] <- 1
+  constraint <- rep(0, m)
+  constraint[(i - 1) * m + 1:(i * m)] <- 1
   add.constraint(lp_model, constraint, ">=", 1)
 }
 
-# Add constraints for standard error
-for (j in 1:m) {
-  constraint <- matrix(0, nrow = 1, ncol = m)
-  constraint[1, j] <- 1
-  add.constraint(lp_model, constraint, "<=", epsilon * sqrt(sum(cij[, j])))
-}
+# Calculate the standard error constraint
+se_constraint <- sqrt(colSums(cij^2)) / sqrt(sum(cij^2 * tj))
+se_constraint_extended <- rep(se_constraint, each = n)
 
-# Set the variable bounds
-for (i in 1:m) {
-  set.bounds(lp_model, lower = 0, upper = 1, columns = i)
-}
+# Add constraint for standard error
+add.constraint(lp_model, se_constraint_extended, "<=", epsilon)
 
-# Solve the LP model
+# Set variable type (binary)
+set.type(lp_model, 1:m, "binary")
+
+# Solve the LP problem
 solve(lp_model)
 
-# Extract the selected domain IDs
-selected_domains <- which(get.variables(lp_model) == 1)
-selected_domains <- paste("Domain", selected_domains, sep = "")
+# Check if the LP problem is feasible
+if (get.status(lp_model) != 0) {
+  stop("No feasible solution found.")
+}
 
-# Output the results
-cat("Selected Domains =", selected_domains, "\n")
+# Get the solution
+solution <- get.variables(lp_model)
+
+# Get the selected domains for each respondent
+Di <- lapply(1:n, function(i) which(solution[(i - 1) * m + 1:(i * m)] > 0))
+
+# Calculate the standard error for each respondent
+se <- sapply(Di, function(d) {
+  if (length(d) > 0) {
+    sqrt(sum(tj[d]) / length(d))
+  } else {
+    NaN
+  }
+})
+
+# Output the selected domains and standard error for each respondent
+for (i in 1:n) {
+  cat("Respondent", i, ": Selected Domains =", Di[[i]], "Standard Error =", se[i], "\n")
+}
